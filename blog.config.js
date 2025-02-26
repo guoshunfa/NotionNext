e4e1caf40e0cf8129af93ac7842c817ffa144f57
guoshunfa <16603684608@guoshunfa.com>
2025-02-27 00:59:46 +0800

    测试hexo

--- blog.config.js
+++ blog.config.js
@@ -5,7 +5,10 @@ const BLOG = {
   NOTION_PAGE_ID:
     process.env.NOTION_PAGE_ID ||
     '1a6c294f1b538088ade6e18a7999acad,en:7c1d570661754c8fbc568e00a01fd70e',
-  THEME: process.env.NEXT_PUBLIC_THEME || 'nav', // 当前主题，在themes文件夹下可找到所有支持的主题；主题名称就是文件夹名，例如 example,fukasawa,gitbook,heo,hexo,landing,matery,medium,next,nobelium,plog,simple
+
+  // nav 主题：nav
+  // heo 主题：博客
+  THEME: process.env.NEXT_PUBLIC_THEME || 'hexo', // 当前主题，在themes文件夹下可找到所有支持的主题；主题名称就是文件夹名，例如 example,fukasawa,gitbook,heo,hexo,landing,matery,medium,next,nobelium,plog,simple
   LANG: process.env.NEXT_PUBLIC_LANG || 'zh-CN', // e.g 'zh-CN','en-US'  see /lib/lang.js for more.
   SINCE: process.env.NEXT_PUBLIC_SINCE || 2019, // e.g if leave this empty, current year will be used.
 
